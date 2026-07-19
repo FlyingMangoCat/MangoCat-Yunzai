beb3b885e55e8c64fd36fd6c68216cc772ea3d00
User <fmc@liulian-ai.top>
2026-07-20 06:40:31 +0800

    fix: initMysUser 当 ltuids 为空时从 MysUserDB 按 UID 补充绑定关系

--- plugins/genshin/model/mys/NoteUser.js
+++ plugins/genshin/model/mys/NoteUser.js
@@ -115,6 +115,33 @@ export default class NoteUser extends BaseModel {
         this.mysUsers[ltuid] = mys
       }
     }
+    // ltuids 为空时，尝试从 MysUserDB 按 games 中的 UID 补充绑定关系
+    if (!ltuids && this.db?.games) {
+      let games = this.db.games
+      let allUids = []
+      MysUtil.eachGame(game => {
+        let ds = games[game]
+        if (ds?.uid) allUids.push(ds.uid)
+        if (ds?.data) {
+          lodash.forEach(ds.data, d => { if (d?.uid) allUids.push(d.uid) })
+        }
+      })
+      if (allUids.length > 0) {
+        let dbs = await MysUserDB.findAll()
+        for (let db of dbs) {
+          let uids = db.uids || {}
+          for (let game of ['gs', 'sr', 'zzz']) {
+            for (let uid of (uids[game] || [])) {
+              if (allUids.includes(uid + "")) {
+                let mys = await MysUser.create(db.ltuid, db)
+                if (mys) this.mysUsers[db.ltuid] = mys
+                break
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   // 保存到数据库
