f394054a03386ad0f1324e70fd0a1e66a2cef9a5
User <fmc@liulian-ai.top>
2026-07-20 18:03:58 +0800

    update: 复制喵喵 Player/Character/Base 组件，支持头像名片显示

--- plugins/genshin/model/Player.js
+++ plugins/genshin/model/Player.js
@@ -1,36 +1,108 @@
 /**
- * Player 类
- * 提供游戏内玩家信息查询
+ * 用户数据文件
+ * 数据存储在/data/UserData/${uid}.json 下
+ * 兼容处理面板户数及Mys数据
+ *
  * 参照喵喵 plugin 的 Player 实现
  */
-export default class Player {
-  constructor(uid, game = "gs") {
+import lodash from 'lodash'
+import Base from './MiaoBase.js'
+import { Data } from '../components/index.js'
+import { Character } from './Character.js'
+
+Data.createDir('/data/UserData', 'root')
+Data.createDir('/data/PlayerData/gs', 'root')
+Data.createDir('/data/PlayerData/sr', 'root')
+
+export default class Player extends Base {
+  constructor (uid, game = 'gs') {
+    super()
+    uid = uid?._mys?.uid || uid?.uid || uid
+    if (!uid) {
+      return false
+    }
+    let cacheObj = this._getCache(`player:${game}:${uid}`)
+    if (cacheObj) {
+      return cacheObj
+    }
     this.uid = uid
     this.game = game
-    this._avatars = {}
-    this.face = ""
+    this.reload()
+    return this._cache(100)
   }
 
-  get isGs() {
-    return this.game === "gs"
+  get hasProfile () {
+    let ret = false
+    this.forEachAvatar((avatar) => {
+      if (avatar.isProfile) {
+        ret = true
+        return false
+      }
+    })
+    return ret
   }
 
-  static create(uid, game = "gs") {
-    return new Player(uid, game)
+  get _file () {
+    return `/data/PlayerData/${this.game}/${this.uid}.json`
   }
 
-  get name() {
-    return ""
-  }
-
-  get level() {
-    return ""
-  }
-
-  get faceImgs() {
-    return {
-      face: "common/item/face.webp",
-      banner: `meta-${this.game}/character/common/imgs/banner.webp`
+  // 玩家头像
+  get faceImgs () {
+    let char
+    if (this.isGs && this.face) {
+      char = Character.get(this.face)
     }
+    if (!char) {
+      let charId = lodash.keys(this._avatars)[0]
+      if (charId) {
+        char = Character.get(charId)
+      }
+    }
+    let imgs = char?.imgs || {}
+    return {
+      face: imgs.face || 'common/item/face.webp',
+      banner: imgs.banner || `meta-${this.game}/character/common/imgs/banner.webp`
+    }
+  }
+
+  static create (e, game = 'gs') {
+    if (e?._mys?.uid || e.uid) {
+      let player = new Player(e?._mys?.uid || e.uid, (game === 'sr' || e.isSr) ? 'sr' : 'gs')
+      player.e = e
+      return player
+    } else {
+      return new Player(e, game)
+    }
+  }
+
+  // 获取面板更新服务名
+  static getProfileServName (uid, game = 'gs') {
+    return null
+  }
+
+  static delByUid (uid, game = 'gs') {
+    let player = Player.create(uid, game)
+    if (player) {
+      player.del()
+    }
+  }
+
+  /**
+   * 重新加载json文件
+   */
+  reload () {
+    let data = Data.readJSON(this._file, 'root')
+    this.setBasicData(data)
+  }
+
+  // 设置基础数据
+  setBasicData (ds) {
+    if (!ds) return
+    this.face = ds.face || this.face || ''
+    this._avatars = ds.avatars || this._avatars || {}
+  }
+
+  del () {
+    this._delCache()
   }
 }
