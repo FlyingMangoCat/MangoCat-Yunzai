e73b8075a299fb74d00daaf293bb6ca2a29de262
fmc <fmc@liulian-ai.top>
2026-08-01 15:25:26 +0800

    fix: base.js screenData 改回原生结构,删写死的枫原万叶
    参考内容 Miao-Yunzai base.js:
    - 星铁用 黑天鹅 立绘做 headImg
    - 原神用 闲云 立绘做 headImg
    - tplFile/pluResPath 按 isSr 分原神/星铁两套
    删掉写死的 let headImg = '枫原万叶' 和拼 namecard 那套
    liulianResPath 由 runtime.render 自动注入,不在 screenData 里

--- plugins/genshin/model/base.js
+++ plugins/genshin/model/base.js
@@ -17,20 +17,34 @@ export default class base {
    * @param pluResPath 插件资源路径
    */
   get screenData() {
-    let headImg = "枫原万叶";
     const layoutPath = `${this._path}/plugins/genshin/resources/html/layout/`;
-
-    return {
+    const baseData = {
       saveId: this.userId,
       cwd: this._path,
       genshinLayout: layoutPath + "genshin.html",
       defaultLayout: layoutPath + "default.html",
+    };
+    if (this.e?.isSr) {
+      return {
+        ...baseData,
+        tplFile: `./plugins/genshin/resources/StarRail/html/${this.model}/${this.model}.html`,
+        /** 绝对路径 */
+        pluResPath: `${this._path}/plugins/genshin/resources/StarRail/`,
+        srtempFile: "StarRail/",
+        /** 星铁头部背景图（黑天鹅立绘） */
+        headImg: `../../liulian-plugin/resources/星铁/splash/黑天鹅.webp`,
+        game: "sr",
+      };
+    }
+    return {
+      ...baseData,
       tplFile: `./plugins/genshin/resources/html/${this.model}/${this.model}.html`,
       /** 绝对路径 */
       pluResPath: `${this._path}/plugins/genshin/resources/`,
-      /** liulian-plugin 资源路径（统一资源复用） */
-      liulianResPath: `${this._path}/plugins/liulian-plugin/resources/`,
-      headStyle: `<style> .head_box { background: url(${this._path}/plugins/genshin/resources/img/namecard/${headImg}.png) #fff; background-position-x: 42px; background-repeat: no-repeat; background-size: auto 101%; }</style>`,
+      srtempFile: "",
+      /** 原神头部背景图（闲云立绘） */
+      headImg: `../../liulian-plugin/resources/genshin/logo/splash/闲云.png`,
+      game: "gs",
     };
   }
 }
